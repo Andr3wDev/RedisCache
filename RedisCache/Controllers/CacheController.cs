0bbd07641662bf455cccbc186ef99206945aa3f9
Andr3wDev <29abrown@gmail.com>
2022-03-04 19:44:50 +1300

    setup redis cache with endpoint

---
+++ RedisCache/Controllers/CacheController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+using System;
+
+namespace RedisCache.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CacheController : ControllerBase
+    {
+        private readonly IMemoryCache memoryCache;
+        public CacheController(IMemoryCache memoryCache)
+        {
+            this.memoryCache = memoryCache;
+        }
+
+        [HttpGet("{key}")]
+        public IActionResult GetCache(string key)
+        {
+            string value = string.Empty;
+            memoryCache.TryGetValue(key, out value);
+
+            return Ok(value);
+        }
+
+        // Takes a key and a value
+        [HttpPost]
+        public IActionResult SetCache(CacheRequest data)
+        {
+            var cacheExpiryOptions = new MemoryCacheEntryOptions
+            {
+                // set the actual expiration regardless of sliding
+                AbsoluteExpiration = DateTime.Now.AddMinutes(5),
+                Priority = CacheItemPriority.High,
+                // a cache entry will expire if not used within this time
+                SlidingExpiration = TimeSpan.FromMinutes(2), 
+                Size = 1024,
+            };
+            memoryCache.Set(data.key, data.value, cacheExpiryOptions);
+
+            return Ok();
+        }
+
+        public class CacheRequest
+        {
+            public string key { get; set; }
+            public string value { get; set; }
+        }
+    }
+}
---
+++ RedisCache/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using RedisCache.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace RedisCache.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
